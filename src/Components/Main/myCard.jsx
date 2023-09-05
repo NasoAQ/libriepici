33443d94b925de44dd04b5dce86a434da4a535b0
naso <naso@epicode.it>
2023-09-05 09:55:13 +0200

    bordoRosso

--- src/Components/Main/myCard.jsx
+++ src/Components/Main/myCard.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import { nanoid } from 'nanoid';
+import "../Main/border.css"
 
 
 class MyCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false,
+    };
+  }
+
   render() {
     const { book } = this.props;
 
     return (
-      <div key={nanoid()} className="col-md-3 mb-4">
+      <div key={nanoid()} className="col-md-3 my-4">
             
       <Card border="warning" className="h-100">
-        <Card.Img variant="top" src={book.img} />
+        <Card.Img
+          variant="top"
+          src={book.img}
+          className={ `${this.state.selected ? 'selected': "" }` }
+          onClick={this.toggleSelected}
+        />
         <Card.Body>
           <Card.Title>{book.title}</Card.Title>
           <Card.Text>
@@ -21,6 +34,11 @@ class MyCard extends Component {
       </Card>
       </div>
     );
+  }
+  toggleSelected = () => {
+    this.setState((prevState) => ({
+      selected: !prevState.selected,
+    }))
   }
 }
 
